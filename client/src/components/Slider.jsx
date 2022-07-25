abd666e2b8999ccbf45e2a9bee4ecaa75b9a85c8
SanjayJPrakash <sanjayjprakash8@gmail.com>
2022-07-25 12:23:44 +0530

    Changed WIP Admin page and changed slider CSS

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -11,7 +11,8 @@ const Container = styled.div`
   display: flex;
   position: relative;
   overflow: hidden;
-  ${mobile({ display: "none" })}
+  // ${mobile({ display: "none" })}
+  
 `;
 
 const Arrow = styled.div`
@@ -37,7 +38,7 @@ const Wrapper = styled.div`
   height: 100%;
   display: flex;
   transition: all 1.5s ease;
-  transform: translateX(${(props) => props.slideIndex * -100}vw);
+  transform: translateX(${(props) => props.slideIndex * -100}vw); 
 `;
 
 const Slide = styled.div`
