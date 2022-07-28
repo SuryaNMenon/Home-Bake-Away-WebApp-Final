b226115f80ce35327cecb986cc365104ba31642b
Surya <suryanmenon@ieee.org>
2022-07-28 12:15:29 +0530

    Changed cart userid reference.

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -47,9 +47,9 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
 });
 
 //GET USER CART
-router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+router.get("/find/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
-    const cart = await Cart.findOne({ userId: req.params.userId });
+    const cart = await Cart.findOne({ userId: req.params.id });
     res.status(200).json(cart);
   } catch (err) {
     res.status(500).json(err);
