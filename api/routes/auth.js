55142d050ef2840c1850410e22974a008e5a2b72
i.vinu <vinayaknaveen01@gmail.comm>
2022-07-28 17:01:37 +0530

    work in progress register

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,6 +5,8 @@ const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
+  try{
+
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
@@ -14,9 +16,9 @@ router.post("/register", async (req, res) => {
     ).toString(),
   });
 
-  try {
+  
     const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
+    res.status(200).json(savedUser);
   } catch (err) {
     res.status(500).json(err);
   }
